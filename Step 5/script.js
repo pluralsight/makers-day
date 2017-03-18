bab541203bc59510d106729c7efd4dd9e7d9061d
Michael Kushlan <michael-kushlan@pluralsight.com>
2017-03-18 15:20:43 -0600

    Test commit

--- Step 5/script.js
+++ Step 5/script.js
@@ -3,5 +3,8 @@ document.getElementById('signup-form').addEventListener("submit", submitForm)
 function submitForm(evt) {
   evt.preventDefault()
 
-  alert("Email " + document.getElementById('email').value + "\nDescription: " + document.getElementById("description").value)
+  var email = document.getElementById('email').value
+  var description = document.getElementById("description").value
+
+  alert("Email " + email + "\nDescription: " + description)
 }
